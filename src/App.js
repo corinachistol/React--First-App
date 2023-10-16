5ede8eb0163819668009507ba07f4c1ca9cb7f34
corinachistol <chistolcorina19@gmail.com>
2023-10-16 16:36:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return <div className="App">
+
+      <h1>First React JS App </h1>
+      
     </div>
-  );
+  }
 }
 
+//main app component
+// function App() {
+//   //JSX syntax below
+//   return (
+//     <div className="App">
+      
+//       <h1>First React JS App</h1>
+
+//     </div>
+//   );
+// }
+
 export default App;
+
+//compoact ,dar neprofesionist
+//element.innerHTML = '<div></div>'
+
+//profesionist, dar nu-i compact in Vanilla
+//element.appendChild( document.createElement( 'div') )
+
+//JSX - lib ne permite sa scrim cod compact, si sub capota sta un cod lung --> datorita caruia combina cele doua metode de mai sus
+
+// HTM: like syntax + DOM OOP 
+
